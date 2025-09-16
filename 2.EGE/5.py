7a0cec74fce18c80a58c1e04c955ffd643da66d5
HrapD <pik722326@gmail.com>
2025-09-16 17:34:53 +0700

    5 номер

--- 2.EGE/5.py
+++ 2.EGE/5.py
@@ -8,6 +8,60 @@
 '''
 # endregion Место для вашего конспекта ⬆️
 
+# Переводы в различные системы исчисления
+"""
+# Универсальная функция перевода 
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r += alp[n % b]
+        n //= b
+    return r[::-1]
+
+n = 8  # 1000
+
+# Перевод в двоичную запись
+print(bin(n)[2:])  # '1000'
+print(f'{n:b}')  # '1000'
+print(int('1000', 2))  #
+
+
+# Перевод в восьмеричную запись
+print(oct(n)[2:])  # '10'
+print(f'{n:o}')  # '10'
+print(int('10', 8))  # 8
+
+
+# Перевод в шестнадцатеричную запись
+print(hex(n)[2:])  # '8'
+print(f'{n:x}')  # '8'
+print(int('8', 16))  # 8
+# ValueError: int() base must be >= 2 and <= 36, or 0
+"""
+# Примеры переводов
+"""
+n = 10**9
+alp = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+def convert(n, b):
+    r = ''
+    while n > 0:
+        r += alp[n % b]
+        n //= b
+    return r[::-1]
+print(convert(n, 2))  # 111011100110101100101000000000
+print(int('111011100110101100101000000000', 2))  # 10**9
+
+print(convert(n, 8))  # 7346545000
+print(int('7346545000', 8))  # 10**9
+
+print(convert(n, 16))  # 3B9ACA00
+print(int('3B9ACA00', 16))  # 10**9
+
+print(convert(n, 3))  # 2120200200021010001
+print(int('2120200200021010001', 3))  # 10**9
+"""
+
 # Домашка 5 номер: https://stepik.org/lesson/1038432/step/1?unit=1060804
 
 # Практика 5 номер: https://stepik.org/lesson/1228668/step/1?unit=1242201
